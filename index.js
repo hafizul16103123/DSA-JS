39e0297cbceee4f03447e6d89f99f6b1851bae6b
Hafiz <islamhafizul158@gmail.com>
2023-03-04 18:08:06 +0600

    sum of all non interger to n

---
+++ index.js
@@ -0,0 +1,11 @@
+
+/**
+ * problem - 1:
+ * Write a recursive function that takes an input n sums all nonnegative integers up to n.
+ * 
+ */
+function doSumRecursively(n) {
+    if (n == 1) return n
+    return n + doSumRecursively(n - 1)
+}
+console.log(doSumRecursively(4))
